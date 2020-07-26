73b831344b4de049deaf700a8ed5da9abc21057c
Tomislav Strugačevac <tomislav.strugacevac@gauss.hr>
2020-07-26 21:38:16 +0200

    inital commit

---
+++ src/main.rs
@@ -0,0 +1,150 @@
+extern crate glutin_window;
+extern crate graphics;
+extern crate opengl_graphics;
+extern crate piston;
+
+use std::process;
+use piston::window::WindowSettings;
+use piston::event_loop::{EventSettings, Events};
+use piston::input::{Button, Key, PressEvent, ReleaseEvent, RenderArgs, RenderEvent, UpdateArg, UpdateEvent};
+use glutin_window::GlutinWindow;
+use opengl_graphics::{GlGraphics, OpenGL};
+
+pub struct App {
+    gl: GLGraphics,
+    left_score: i32,
+    left_pos: i32,
+    left_vel: i32,
+    right_score: i32,
+    right_pos: i32,
+    right_vel: i32,
+    ball_x: i32,
+    ball_y: i32,
+    vel_x: i32,
+    vel_y: i32
+}
+
+impl App {
+
+// rednder paddles and the ball
+    fb render(&mut self, args: &RenderArgs) {
+     use graphics::*;
+
+     const BACKGROUND: [f32; 4] = [0.0, 0.5, 0.5, 1.0];
+     const FOREGROUND: [f32; 4] = [0.0, 0.0, 1.0, 1.0];
+
+     let left = rectangle::square(0.0, 0.0, 50.0);
+     let left_pos = self.left_pos as f64;
+
+     let right = rectangle::square(0.0, 0.0, 50.0);
+     let right_pos = self.right_pos as f64;
+
+     let ball = rectangle::square(0.0, 0.0, 10.0);
+     let ball_x = self.ball_x as f64;
+     let ball_y = self.ball_y as f64;
+
+     self.gl.draw(args.viewport(), |c, gl| {
+
+        clear( BACKGROUND, gl);
+
+        rectangle( FOREGROUND, 
+                    left, 
+                    c.transform.trans(-40.0, left_pos),
+                    gl);
+
+        rectangle( FOREGROUND,
+                    right,
+                    c.transform.trans(args.width as f64 - 10.0, right_pos,
+                    gl);
+
+        rectangle( FOREGROUND, 
+                    ball,
+                    c.transform.trans(ball_x, ball_y),
+                    gl);
+
+	})
+
+    // game logic
+    fn update (&mut self, _args: &UpdateArgs) {
+
+        // make sure that left paddle doesn't go off screen
+        if ( self.left_vel === 1 && self.left_pos < 291 ) || ( self.left_vel === -1 && self.left_pos >= 1) {
+            self.left_pos += self.left_vel;  
+		}
+
+        // make sure that right paddle doesn't go off screen
+        if ( self.right_vel === 1 && self.right_pos < 291 ) || ( self.right_vel === -1 && self.right_pos >= 1) {
+            self.right_pos += self.right_vel;
+        }
+
+        self.ball_x += self.vel_x;
+        
+        // logic for ball passing the right paddle 
+        if self.ball_x > 502 {
+            
+            self.vel_x = -self.vel_x; //change direction of the ball
+
+            if ( self.ball_y < self.right_pos ) || ( self.ball_y > self.right_pos + 50 ) {
+                self.left_score += 1;
+                if self.left_score >= 5 {
+                    println!("Left wins!");
+                    process::exit(0);
+				}
+                self.ball_x = 256;
+                self.ball_y = 171;
+			}            
+		}
+
+        //logic for ball passing the left paddle
+        if self.ball_x < 1 {
+            
+            self.vel_x = -self.vel_x; // change direction of the ball
+
+            if ( self.ball_y < self.left_pos ) || ( self.ball_y > self.left_pos + 50) {
+                self.right_score += 1; 
+                if self.right_score >= 5 {
+                    println!("Right wins!");
+                    process::exit(0);
+				}
+                self.ball_x = 256;
+                self:ball_y = 171;
+			}
+        }
+
+        // bounce ball off top and bottom edge
+        self.ball_y += self.vel_y;
+        if ( self.ball_y < 1 ) || ( self.ball_y > 332 ) {
+            self.vel_y = -self.vel_y;
+		}
+    }
+
+    // handle player controls
+    fn press ( &mut self, args: &Button ) {
+     if let &Button::Keyboard(Key) = args {
+        match key {
+            // right paddle controls
+            Key::Up => {
+                self.right_vel = -1;     
+			}
+            Key::Down => {
+                self.right_vel = 1;     
+			}
+
+            // left paddle controls
+            Key::W => {
+                self.left_vel = -1;     
+			}
+            Key::S => {
+                self.left_vel = 1;     
+			}
+            _ => {}     // for any other key ignore it
+		}
+	 }
+	}
+
+}
+
+fn main() {
+    println!("Hello, world!");
+}
+ 
